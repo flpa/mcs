697b5881a6854f6ff978296756cb774b8f9c7213
Florian Patzl <if14b048@technikum-wien.at>
2015-11-14 13:19:05 +0100

    Use Java 8 in Main to ensure we're running with Java8

--- src/main/java/at/fhtw/mcs/Main.java
+++ src/main/java/at/fhtw/mcs/Main.java
@@ -1,8 +1,21 @@
 package at.fhtw.mcs;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Main {
+	
 	public static void main(String[] args) {
-		System.out.println("Hallo Josh und Ralf.");
+		/*
+		 * Don't try this at home kids, just using a random Java 8 feature
+		 * to ensure Gradle runs with J8.
+		 */
+		
+		System.out.println("Hallo " + 
+				Arrays.stream(new String[] { "Josh", "Ralf" })
+					  .map(name -> "'" + name + "'")
+					  .collect(Collectors.joining(" und ")) 
+				+ ".");
 	}
 
 	public int testedMethod(int n) {
