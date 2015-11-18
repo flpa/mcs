99994f21170a9622941e836a46922aea1eec1add
Florian Patzl <if14b048@technikum-wien.at>
2015-11-18 15:53:41 +0100

    Add MCS Eclipse Formatter

--- src/main/java/at/fhtw/mcs/Main.java
+++ src/main/java/at/fhtw/mcs/Main.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class Main {
-	
+
 	public static void main(String[] args) {
 		/*
-		 * Don't try this at home kids, just using a random Java 8 feature
-		 * to ensure Gradle runs with J8.
+		 * Don't try this at home kids, just using a random Java 8 feature to
+		 * ensure Gradle runs with J8.
 		 */
-		
+
+		//@formatter:off
 		System.out.println("Hallo " + 
 				Arrays.stream(new String[] { "Josh", "Ralf" })
 					  .map(name -> "'" + name + "'")
 					  .collect(Collectors.joining(" und ")) 
 				+ ".");
+		//@formatter:on
 	}
 
 	public int testedMethod(int n) {
